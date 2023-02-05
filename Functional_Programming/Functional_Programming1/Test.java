7c849bf5a86bb6d16a697b1adcaff8793ddb38b4
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-02-05 18:49:25 +0530

    Update Test.java

--- Functional_Programming/Functional_Programming1/Test.java
+++ Functional_Programming/Functional_Programming1/Test.java
@@ -1,16 +1,40 @@
-package mathoperationwithlambda;
-    public class Test implements MathOperation {
-        public static void main(String[] args) {
-            MathOperation addition= (int a, int b)-> a+b;
-            MathOperation subtraction= (int a, int b)-> a-b;
-            MathOperation multiplication= (int a, int b)-> {return a*b;};
-            MathOperation division= (int a, int b)-> a/b;
-            System.out.println("10 + 25 = "+operate(10, 25, addition));
-            System.out.println("10 - 25 = "+operate(10, 25, subtraction));
-            System.out.println("10 * 25 = "+operate(10, 25, multiplication));
-            System.out.println("10 / 25 = "+operate(10, 25, division));
-        }
-        static private int operate(int a, int b, MathOperation mathOperation){
-            return mathOperation.operation(a, b);
-        }
-    } 
+//package mathoperationwithlambda;
+
+@FunctionalInterface
+public interface MathOperation{
+    int operation(int a, int b);
+
+}
+
+public abstract class Test implements MathOperation {
+    public static void main(String[] args) {
+        MathOperation addition= (int a, int b)-> a+b;
+        MathOperation subtraction = (int a, int b)-> a-b;
+        MathOperation multiplication= (int  a, int b)-> {return a*b;};
+        MathOperation division = (int a, int b) -> {
+            if (b == 0) {
+                System.out.println("Cannot divide by zero.");
+                return 0;
+            }
+            return a / b;
+        };
+        System.out.println("10 + 25 = " + operate(10, 25, addition));
+        System.out.println("10 - 25 = " + operate(10, 25, subtraction));
+        System.out.println("10 * 25 = " + operate(10, 25, multiplication));
+        System.out.println("10 / 25 = " + operate(10, 25, division));
+    }
+    static private int operate(int a, int b, MathOperation mathOperation){
+        return mathOperation.operation(a, b);
+    }
+} 
+
+
+/*
+Output:
+10 + 25 = 35
+10 - 25 = -15
+10 * 25 = 250
+10 / 25 = 0
+Cannot divide by zero.
+
+*/ 
